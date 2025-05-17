4cc9079d10a93ebcdb009af90c94c55d75dcb734
Sachin <sachinkaushikofficial@gmail.com>
2025-05-17 19:18:30 +0530

    Adding PreAuthorize to secure methods based on role access

--- src/main/java/com/sachinka/spring_security_project/controller/MyController.java
+++ src/main/java/com/sachinka/spring_security_project/controller/MyController.java
@@ -1,5 +1,6 @@
 package com.sachinka.spring_security_project.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,11 +13,13 @@ public class MyController {
         return "Hello";
     }
 
+    @PreAuthorize("hasRole('USER')")
     @RequestMapping(value = "/user", method = RequestMethod.GET)
     public String greetUser() {
         return "Hello, User";
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "/admin", method = RequestMethod.GET)
     public String greetAdmin() {
         return "Hello, Admin";
