74394567ec64ea457e35207531899dc1c3cc4b2e
Sachin <sachinkaushikofficial@gmail.com>
2025-05-17 18:03:37 +0530

    Initial commit

---
+++ src/main/java/com/sachinka/spring_security_project/controller/MyController.java
@@ -0,0 +1,14 @@
+package com.sachinka.spring_security_project.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String greet() {
+        return "Hello";
+    }
+}
